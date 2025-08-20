5ca38df84d49500fcc61e44466397b2647063494
9helix <grzinicdino@gmail.com>
2025-08-20 13:37:45 +0200

    chore: add more precise star coordinates

--- lib/stars.dart
+++ lib/stars.dart
@@ -2,9 +2,9 @@ import 'coordinates.dart';
 
 enum FovStars {
   //summer triangle
-  deneb('Deneb', Coordinates(310, 45)),
-  altair('Altair', Coordinates(298, 9)),
-  vega('Vega', Coordinates(279, 39)),
+  deneb('Deneb', Coordinates(310.25, 45.27)),
+  altair('Altair', Coordinates(297.5, 8.87)),
+  vega('Vega', Coordinates(279.0, 38.78)),
 
   arcturus('Arcturus', Coordinates(214, 19)),
   antares('Antares', Coordinates(247, 26.42)),
@@ -24,16 +24,21 @@ enum FovStars {
   hamal('Hamal', Coordinates(31.75, 23.45)),
 
   // winter hexagon
-  rigel('Rigel', Coordinates(79, -8)),
-  sirius('Sirius', Coordinates(101, -17)),
-  procyon('Procyon', Coordinates(115, 5)),
-  pollux('Pollux', Coordinates(116, 28)),
-  capella('Capella', Coordinates(79, 46)),
-  aldebaran('Aldebaran', Coordinates(69, 17)),
-  castor('Castor', Coordinates(114, 32)),
+  rigel('Rigel', Coordinates(78.5, -8.2)),
+  sirius('Sirius', Coordinates(101.25, -16.7)),
+  procyon('Procyon', Coordinates(114.75, 5.22)),
+  pollux('Pollux', Coordinates(116.25, 28.02)),
+  capella('Capella', Coordinates(79.0, 45.98)),
+  aldebaran('Aldebaran', Coordinates(68.75, 16.5)),
+  castor('Castor', Coordinates(113.5, 31.88)),
   ;
 
   const FovStars(this.star, this.coords);
   final String star;
   final Coordinates coords;
+
+  // Static method to convert enum to map
+  static Map<String, Coordinates> toMap() {
+    return {for (var star in FovStars.values) star.star: star.coords};
+  }
 }
